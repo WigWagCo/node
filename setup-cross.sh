b7786af98e444319d40d99e6c4b2575509337308
edhemphill <ed@wigwag.com>
2014-07-25 18:09:44 -0500

    better setup-cross.sh - any command

--- setup-cross.sh
+++ setup-cross.sh
@@ -9,12 +9,22 @@ fi
 
 source "$1"
 
+
+if [ "$#" -lt 2 ]; then
+    CMD="default"
+fi
+
 echo "Sanity check:"
 echo "CC=$CC"
 echo
 
+if [ "$CMD" == "default" ]; then
 #--shared-zlib --shared-zlib-libpath=${ROOTFS_USR_LIB} --shared-zlib-includes=${ROOTFS_USR_INC} --debug
 ./configure  --without-snapshot --dest-cpu=${NODE_DEST_CPU} --gdb  --without-ssl --no-ssl2 # --shared-v8
 bash --norc
+else 
+shift
+$@
+fi
 
 # or run make / make -j 8 / etc...
